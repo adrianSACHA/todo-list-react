bc5050743852b010e572ed0f7fa6356adfdbe56f
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2022-11-15 13:22:23 +0100

    addind section to react

---
+++ src/Form/index.js
@@ -0,0 +1,15 @@
+import "./style.css"
+
+const Form = () => (
+  <form className="js-form form">
+    <input
+      className="form__input"
+      autofocus
+      placeholder="Co jest do zrobienia?"
+      type="text"
+    />
+    <button className="form__button">Dodaj zadanie</button>
+  </form>
+);
+
+export default Form;
