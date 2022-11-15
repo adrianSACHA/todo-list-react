a7a5cf5d2fea846442b2cd274f2347195e152a2a
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2022-11-15 13:21:48 +0100

    transfering my todo list to react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from "./Form";
+import Tasks from "./Tasks";
+
+const tasks = [
+  {id: 1, content: "przejść na REACT", done: false }, 
+  {id: 2, content: "zjeść obiad", done: true },
+];
+
+const hideDoneTasks = false;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+        <header>
+            <h1 className="container__header">Lista zadań</h1>
+        </header>
+        <section className="section">
+            <h2 className="section__header">Dodaj nowe zadanie</h2>
+            <div className="section__body">
+                <Form />
+            </div>
+        </section>
+        <section className="section__tasksList">
+            <div className="tasksList">
+                <h2 className="tasksList__header">
+                    Lista zadań
+                </h2>
+                <div className="tasksList__doneButtons">
+                    <div className="tasksList__doneButton">
+                        Ukryj ukończone
+                    </div>
+                    <div className="tasksList__doneButton">
+                        Ukończ wszystkie
+                    </div>
+                </div>
+            </div>
+            <Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />
+        </section>
+    </main>
   );
 }
 
