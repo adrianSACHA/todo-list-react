456480b150823880313324580d25f51dc0174eed
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2022-11-16 13:14:53 +0100

    added React.fragment

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,19 +1,22 @@
+import React from "react";
 import "./style.css";
 
-const Buttons = ({tasks, hideDoneTasks}) => (
-  tasks.length > 0 && (
-    <div className="tasksList__buttons">
-      <button className="tasksList__button">
-        {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
-      </button>
-      <button
-      className="tasksList__button"
-      disabled={tasks.every(({ done }) => done)}
-       >
-        Ukończ wszystkie
-      </button>
-    </div>
-  )
+const Buttons = ({ tasks, hideDoneTasks }) => (
+  <div className="tasksList__buttons">
+    {tasks.length > 0 && (
+      <>
+        <button className="tasksList__button">
+          {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
+        </button>
+        <button
+          className="tasksList__button"
+          disabled={tasks.every(({ done }) => done)}
+        >
+          Ukończ wszystkie
+        </button>
+      </>
+    )}
+  </div>
 );
 
 export default Buttons;
