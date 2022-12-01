b05c81fc21d5a32d73ad22c7e26eeb7dc2ddf441
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2022-12-01 12:19:27 +0100

    styledComponents globalStyle, ThemeProvider, theme

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { theme } from "./theme";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./createGlobalStyle";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle>
+        <App />
+      </GlobalStyle>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
