a1727eeba16fe08edeef0d273b7db43c537f4b19
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2022-12-01 11:36:36 +0100

    deleted brackets in line 5

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -2,13 +2,13 @@ import { List, Item, Content, Button } from "./styled";
 
 const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
   <List>
-    {tasks.map((task) => (
+    {tasks.map(task => (
       <Item key={task.id} hidden={task.done && hideDone}>
         <Button
           toggleDone
           onClick={() => toggleTaskDone(task.id)}
         >
-          {task.done ? "✔" : ""}
+          {task.done ? " ✔" : ""}
         </Button>
         <Content done={task.done}>{task.content}</Content>
         <Button remove onClick={() => removeTask(task.id)}>
