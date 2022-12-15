e5361c7c26fd941d36f4d7cee647358bb1ac9dff
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2022-12-15 12:02:07 +0100

    imported store and Provider from react redux

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ import { theme } from "./theme";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "styled-components";
+import { Provider } from "react-redux";
 import { GlobalStyle } from "./GlobalStyle";
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
