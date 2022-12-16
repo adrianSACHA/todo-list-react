415e6d602a9c938e88ff6156fdb9dca3e175b721
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2022-12-16 11:29:47 +0100

    formatted and updated, removed useSelector

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -1,51 +1,23 @@
-import { useSelector } from "react-redux";
 import { Container } from "../../common/Container/styled";
 import Form from "./Form";
 import TaskList from "./TaskList";
 import Buttons from "./Buttons";
 import Section from "../../common/Section";
 import Header from "../../common/Header";
-import useTasks from "../../useTasks";
-import { selectTasks } from "./tasksSlice";
 
 function Tasks() {
-  const { tasks } = useSelector(selectTasks);
-
-  const {
-    // hideDone,
-    // tasks,
-    setAllDone,
-    addNewTask,
-    toggleHideDone,
-    toggleTaskDone,
-    removeTask,
-  } = useTasks();
 
   return (
     <Container>
       <Header title="Lista zadań" />
       <Section
         title="Dodaj nowe zadanie"
-        body={<Form addNewTask={addNewTask} />}
+        body={<Form />}
       />
       <Section
         title="Lista zadań"
-        body={
-          <TaskList
-            tasks={tasks}
-            // hideDone={hideDone}
-            removeTask={removeTask}
-            toggleTaskDone={toggleTaskDone}
-          />
-        }
-        extraHeaderContent={
-          <Buttons
-            tasks={tasks}
-            // hideDone={hideDone}
-            // toggleHideDone={toggleHideDone}
-            setAllDone={setAllDone}
-          />
-        }
+        body={<TaskList />}
+        extraHeaderContent={<Buttons />}
       />
     </Container>
   );
