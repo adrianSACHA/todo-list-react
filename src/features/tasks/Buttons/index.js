8eab27f32bb05488ce9d801f72f858fdd1d9b63b
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2022-12-16 10:01:21 +0100

    added useSelector, useDispatch to toggleHideDone

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,15 +1,26 @@
+import { useSelector, useDispatch } from "react-redux";
+import { selectTasks, toggleHideDone } from "../tasksSlice";
 import { StyledButtons, Button } from "./styled";
 
-const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
-  tasks.length > 0 && (
+const Buttons = ({ setAllDone }) => {
+  const { tasks, hideDone } = useSelector(selectTasks);
+  const dispatch = useDispatch();
+  return (
     <StyledButtons>
-      <Button onClick={toggleHideDone}>
-        {hideDone ? "Pokaż" : "Ukryj"} ukończone
-      </Button>
-      <Button disabled={tasks.every(({ done }) => done)} onClick={setAllDone}>
-        Ukończ wszystkie
-      </Button>
-    </StyledButtons>)
+      {tasks.length > 0 && (
+        <>
+          <Button onClick={() => dispatch(toggleHideDone())}>
+            {hideDone ? "Pokaż" : "Ukryj"} ukończone
+          </Button>
+          <Button
+            disabled={tasks.every(({ done }) => done)}
+            onClick={setAllDone}
+          >
+            Ukończ wszystkie
+          </Button>
+        </>
+      )}
+    </StyledButtons>
   );
-
+};
 export default Buttons;
