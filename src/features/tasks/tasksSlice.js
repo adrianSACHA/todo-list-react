5a68f622e4b703598af596293ad346cb32681240
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2022-12-28 10:42:10 +0100

    added more selectors

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -36,6 +36,13 @@ export const {
   removeTask,
   setAllDone,
 } = tasksSlice.actions;
-export const selectTasksState = (state) => state.tasks;
+
+const selectTasksState = (state) => state.tasks;
+
 export const selectTasks = (state) => selectTasksState(state).tasks;
+export const selectHideDone = (state) => selectTasksState(state).hideDone;
+export const selectAreTaskEmpty = (state) => selectTasks(state).length === 0;
+export const selectIsEveryTaskDone = (state) =>
+  selectTasks(state).every(({ done }) => done);
+
 export default tasksSlice.reducer;
