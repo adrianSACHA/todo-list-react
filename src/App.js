86746430c785636e8b8d23a285788a17fecec25b
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2022-12-29 17:41:53 +0100

    HashRouter, Link, Switch, Route, Redirect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
 import React from "react";
-import Tasks from "./features/tasks/Tasks";
+import { HashRouter, Link, Switch, Route, Redirect } from "react-router-dom";
+import TasksPage from "./features/tasks/TasksPage";
+import TaskPage from "./features/tasks/TaskPage";
+import AuthorPage from "./features/author/AuthorPage";
 
-const App = () => <Tasks />;
+const App = () => (
+  <HashRouter basename="/todo-list-react">
+    <nav>
+      <ul>
+        <li>
+          <Link to="/zadania">Zadania</Link>
+        </li>
+        <li>
+          <Link to="/autor">O autorze</Link>
+        </li>
+      </ul>
+      <Switch>
+      <Route path="/zadania/:id">
+          <TaskPage />
+        </Route>
+       <Route path="/zadania">
+          <TasksPage />
+        </Route>
+        <Route path="/autor">
+          <AuthorPage />
+        </Route>
+        <Route path="/">
+          <Redirect to="/zadania" />
+        </Route>
+      </Switch>
+    </nav>
+  </HashRouter>
+);
 
 export default App;
