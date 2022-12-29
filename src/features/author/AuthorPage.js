62b6caa090e9bc6781ff4c2eb65b2543169fb1f2
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2022-12-29 20:07:07 +0100

    updated and formatted

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -13,34 +13,39 @@ const Author = () => (
           <p>
             <strong>Zainteresowania</strong>
           </p>
+          <p>Programowanie na poważnie od kilku miesięcy.</p>
+          Kiedyś tworzyłem stronę internetową z okładkami do albumów z muzyką.
+          Dawało mi to dużo przyjemności.
+          <p>Oprócz tego interesują mnie nowinki technologiczne.</p>
+          Lubię majsterkowanie, coś stworzyć z niczego.
           <p>
-            Programowanie na poważnie od kilku miesięcy.
-            <p>
-              Kiedyś tworzyłem stronę internetową z okładkami do albumów z
-              muzyką. Dawało mi to dużo przyjemności.
-            </p>
-            <p>Oprócz tego interesują mnie nowinki technologiczne.</p> 
-            Lubię
-            majsterkowanie, coś stworzyć zniczego.
-            <p>Układać Lego z synem, ostatnio X-Wing Luka Skywalkera. </p>
+            Układać razem z synem Lego, ostatnim challengem był X-Wing Luka
+            Skywalkera.{" "}
           </p>
           <p>
             <strong>Zawodowo</strong>
           </p>
           <p>
+            Moja przygoda z handlem zaczęła się od Sprzedawcy/Prezentera.
+            Przeszedłem całą możliwą ścieżkę jaką według mnie, każdy powinien
+            przejść zanim zacznie zarządzać ludźmi. Bez tej wiedzy zespół nie
+            będzie za Tobą. Na stanowisku kierowniczym jestem ponad 8-lat.
+          </p>
+          <p>
+            Niecały rok, wdrażałem Managerów na terenie północnej Polski.
+            Przyglądanie się nie działającym procesom to było wyzwanie. Pandemia
+            zmieniła trochę realia i wróciłem na stanowisko kierownika sklepu.
+          </p>
+          Po 12 latach w jednej firmie nadszedł czas zmian, rok 2022 będzie
+          rokiem zmian w moim życiu
+          <p>
             <strong>
-              Moje motto: Zawsze szukam rozwiązań, dlatego jestem w tym miejscu
-              i czasie.
+              Moje motto:{" "}
+              <i>
+                Zawsze szukam rozwiązań, dlatego jestem w tym miejscu i czasie.
+              </i>
             </strong>
           </p>
-          Zaczynałem od sprzedawcy i przeszedłem ścieżkę jaką według mnie każdy
-          powinien przejść, zanim zacznie zarządzać ludźmi. Bez tej wiedzy nie
-          będzie miał za sobą zespołu, którym masz zarządzać. Na stanowisku
-          kierowniczym jestem ponad 8lat. Niecały rok wdrażałem menadżerów na
-          terenie północne Polski. Diagnoza i przyglądanie się nie działającym
-          procesom to było wyzwanie. Pandemia zmieniła trochę realia i wróciłem
-          na stanowisko kierownika sklepu. Po 12 latach w jednej firmie nadszedł
-          czas zmian, rok 2022 będzie rokiem zmian w moim życiu
         </>
       }
     />
