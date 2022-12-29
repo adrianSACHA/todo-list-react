d14abe35d39c3e38abb8b3c15c5e59251361a50e
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2022-12-29 19:37:34 +0100

    search index with input

---
+++ src/features/tasks/TasksPage/Search/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import Input from "../../Input";
+import { Wrapper } from "./styled";
+
+
+const Function = () => {
+  const location = useLocation();
+  const history = useHistory();
+  const query = new URLSearchParams(location.search).get("szukaj");
+ 
+  const onInputChange = ({ target }) => {
+    const searchParams = new URLSearchParams(location.search);
+
+    if (target.value.trim() === "") {
+      searchParams.delete("szukaj");
+    } else {
+      searchParams.set("szukaj", target.value);
+    }
+    history.push(`${location.pathname}?${searchParams.toString()}`);
+  };
+
+  return (
+    <Wrapper>
+    <Input
+      placeholder="Filtruj zadania"
+      value={query || ""}
+      onChange={onInputChange}
+    />
+    </Wrapper>
+  );
+};
+
+export default Function;
