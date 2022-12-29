3314255416db2e2873974fa81b48f2d95100d0f7
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2022-12-29 19:33:58 +0100

    changed padding in Sections and Header

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const Sections = styled.section`
   background-color: ${({ theme }) => theme.color.white};
   margin: 10px auto;
-  padding: 10px 20px;
   box-shadow: 0 0 5px #ddd;
 `;
 
@@ -14,6 +13,7 @@ export const List = styled.header`
   align-items: center;
   border-bottom: 1px solid #ddd;
   margin-bottom: 10px;
+  padding: 5px 5px 5px 20px;
 
   @media (max-width: ${({ theme }) => theme.breakPoint.mobile}px) {
     grid-template-columns: 1fr;
