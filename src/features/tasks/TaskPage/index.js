c6a5de52273af0308876ef48fb87bc1d34aa2d5b
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2022-12-29 17:43:40 +0100

    added index.js to TaskPage, useParams getTaskById

---
+++ src/features/tasks/TaskPage/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Container } from "../../../common/Container/styled";
+import Section from "../../../common/Section";
+import Header from "../../../common/Header";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getTaskById } from "../tasksSlice";
+
+function TaskPage() {
+  const { id } = useParams();
+const task = useSelector(state => getTaskById(state, id));
+
+
+  return (
+    <Container>
+      <Header title="Szczegóły zadania" />
+      <Section title={task ? task.content : "Nie znaleziono zadania"} body={
+      <><strong>Ukończono:</strong> {task.done ? "Tak" : "Nie"}
+      </>} />
+    </Container>
+  );
+}
+
+export default TaskPage;
