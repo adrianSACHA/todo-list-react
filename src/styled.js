e1a1631fe862dcf965dbee75e78fd7a83088a105
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-02 14:46:38 +0100

    added styled to navigation

--- src/styled.js
+++ src/styled.js
@@ -1,8 +1,33 @@
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
+export const Nav = styled.nav`
+display: flex;
+flex-direction: column;
+flex-basis: 100%;
+`;
+
+export const UnorderedList = styled.ul`
+list-style: none;
+display: flex;
+justify-content: center;
+background-color: ${({theme}) => theme.color.teal};
+
+margin: 0px;
+padding: 10px;
+`;
+
+export const List = styled.li`
+padding: 10px;
+`;
+
 export const StyledNavLink = styled(NavLink)`
+text-decoration: none;
+font-size: x-large;
+color: ${({theme}) => theme.color.white};
+
+
   &.active {
-    color: red;
+    color: ${({theme}) => theme.color.silver};;
   }
 `;
