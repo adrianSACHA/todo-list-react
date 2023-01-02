91fd836a06d4f1f7049d27456fd0fc6300a98524
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-02 14:50:05 +0100

    added fetchExampleTask: Error and Succes and delay

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -1,14 +1,15 @@
-import { takeLatest, call, put, takeEvery, select } from "redux-saga/effects";
+import { takeLatest, call, put, takeEvery, select, delay } from "redux-saga/effects";
 import { getExampleTasks } from "./getExampleTasks";
 import { saveTasksInLocalStorage } from "./tasksLocalStorage";
-import { fetchExampleTasks, selectTasks, setTasks } from "./tasksSlice";
+import { fetchExampleTasks, fetchExampleTasksError, fetchExampleTasksSuccess, selectTasks } from "./tasksSlice";
 
 function* fetchExampleTasksHandler() {
   try {
+    yield delay(1000);
     const exampleTasks = yield call(getExampleTasks);
-    yield put(setTasks(exampleTasks));
+    yield put(fetchExampleTasksSuccess(exampleTasks));
   } catch (error) {
-    yield call(alert, "Coś poszło nie tak!");
+    yield put(fetchExampleTasksError());
   }
 }
 
