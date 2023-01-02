48853dddca793dcf0866fba7240e3f0552ad3442
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-02 14:47:32 +0100

    styled Button with loading state

---
+++ src/common/Button/styled.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  color: ${({ theme }) => theme.color.teal};
+  border: none;
+  transition: 0.3s;
+  background: transparent;
+  margin: 0 0 0 20px;
+  cursor: pointer;
+
+  @media (max-width: ${({ theme }) => theme.breakPoint.mobile}px) {
+    flex-basis: 100%;
+    margin: 0;
+  }
+
+  &:hover {
+    color: ${({ theme }) => theme.color.bondiBlue};
+  }
+  &:active {
+    color: ${({ theme }) => theme.color.cyan};
+  }
+
+  &:disabled {
+    color: ${({ theme }) => theme.color.silver};
+  }
+`;
