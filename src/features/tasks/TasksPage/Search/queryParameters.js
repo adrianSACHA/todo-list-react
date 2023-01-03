db360038c50822f1d508327ff2f232a330292b03
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-03 17:00:04 +0100

    created custom hook

---
+++ src/features/tasks/TasksPage/Search/queryParameters.js
@@ -0,0 +1,24 @@
+import { useHistory, useLocation } from "react-router-dom";
+
+export const useQueryParameter = (paramName) => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+
+  return searchParams.get(paramName);
+};
+
+export const useReplaceQueryParameter = (paramName, paramValue) => {
+  const location = useLocation();
+  const history = useHistory();
+
+  return ({ paramName, paramValue }) => {
+    const searchParams = new URLSearchParams(location.search);
+
+    if (paramValue === undefined) {
+      searchParams.delete(paramName);
+    } else {
+      searchParams.set(paramName, paramValue);
+    }
+    history.push(`${location.pathname}?${searchParams.toString()}`);
+  };
+};
