8e32eee97d0feeb0385fbeaf99a24eb3c36bac7b
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-03 13:07:49 +0100

    added gap to navigation, hover and transition

--- src/styled.js
+++ src/styled.js
@@ -2,32 +2,38 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 export const Nav = styled.nav`
-display: flex;
-flex-direction: column;
-flex-basis: 100%;
+  display: flex;
+  flex-direction: column;
+  flex-basis: auto;
+  margin: 0 auto;
 `;
 
 export const UnorderedList = styled.ul`
-list-style: none;
-display: flex;
-justify-content: center;
-background-color: ${({theme}) => theme.color.teal};
-
-margin: 0px;
-padding: 10px;
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.color.teal};
+  margin: 0;
+  padding: 10px;
+  gap: 15px;
 `;
 
 export const List = styled.li`
-padding: 10px;
+  padding: 10px;
 `;
 
 export const StyledNavLink = styled(NavLink)`
-text-decoration: none;
-font-size: x-large;
-color: ${({theme}) => theme.color.white};
+  text-decoration: none;
+  font-size: large;
+  color: ${({ theme }) => theme.color.white};
+  transition: 0.3s;
 
+  &:hover {
+    transform: scale(1.1);
+    font-weight: bold;
+  }
 
   &.active {
-    color: ${({theme}) => theme.color.silver};;
+    font-weight: bold;
   }
 `;
